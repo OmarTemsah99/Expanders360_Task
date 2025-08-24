83e33b827135a75c28c00e1a24bb591671e2c434
omar temsah <omartemsah99@gmail.com>
2025-08-24 19:21:09 +0300

    matches module, services & controllers

--- src/matches/matches.service.ts
+++ src/matches/matches.service.ts
@@ -1,26 +1,21 @@
-import { Injectable } from '@nestjs/common';
-import { CreateMatchDto } from './dto/create-match.dto';
-import { UpdateMatchDto } from './dto/update-match.dto';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Match } from './entities/match.entity';
 
 @Injectable()
 export class MatchesService {
-  create(createMatchDto: CreateMatchDto) {
-    return 'This action adds a new match';
-  }
+  constructor(
+    @InjectRepository(Match) private readonly matches: Repository<Match>,
+  ) {}
 
   findAll() {
-    return `This action returns all matches`;
+    return this.matches.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} match`;
-  }
-
-  update(id: number, updateMatchDto: UpdateMatchDto) {
-    return `This action updates a #${id} match`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} match`;
+  async findOne(id: number) {
+    const m = await this.matches.findOne({ where: { id } });
+    if (!m) throw new NotFoundException('Match not found');
+    return m;
   }
 }
