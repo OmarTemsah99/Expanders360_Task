f7ad3dc5ce6db80831c440826d17bead3054b9bf
omar temsah <omartemsah99@gmail.com>
2025-08-25 11:36:20 +0300

    comments removal

--- src/documents/dto/create-document.dto.ts
+++ src/documents/dto/create-document.dto.ts
@@ -1,7 +1,6 @@
 import { IsString, IsArray, IsOptional, IsUUID } from 'class-validator';
 
 export class CreateDocumentDto {
-  // We're updating the validator to IsUUID()
   @IsUUID()
   projectId: string;
 
